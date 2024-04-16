da730f8a9cf610ebb7af7e5c04f9a1ccc29d4969
jelliijoa <jelliijoa@gmail.com>
2024-04-16 21:45:21 +0900

    [FEAT] 검색 추가 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import SongList from './components/SongList';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
+  const [favoriteSongs, setFavoriteSongs] = useState([]);
+  const [showInitialMessage, setShowInitialMessage] = useState(true);
+
+  useEffect(() => {
+    const favoriteSongsFromStorage = JSON.parse(
+      localStorage.getItem('favoriteSongs')
+    );
+    if (favoriteSongsFromStorage) {
+      setFavoriteSongs(favoriteSongsFromStorage);
+    }
+  }, []);
+
+  const handleFavorite = (song) => {
+    const isFavorited = favoriteSongs.some(
+      (favoriteSong) => favoriteSong.no === song.no
+    );
+    if (isFavorited) {
+      const updatedFavoriteSongs = favoriteSongs.filter(
+        (favoriteSong) => favoriteSong.no !== song.no
+      );
+      setFavoriteSongs(updatedFavoriteSongs);
+    } else {
+      setFavoriteSongs([...favoriteSongs, song]);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('favoriteSongs', JSON.stringify(favoriteSongs));
+  }, [favoriteSongs]);
+
+  const handleSearch = () => {
+    setShowInitialMessage(false);
+  };
 
   return (
-    <>
-      <div className='App'>
-        <SearchBar setSongList={setData} />
-        <SongList data={data} />
-      </div>
-    </>
+    <div className='App'>
+      <SearchBar setSongList={setData} handleSearch={handleSearch} />
+
+      {data ? (
+        <SongList data={data} handleFavorite={handleFavorite} />
+      ) : (
+        <div>노래를 검색해주세요.</div>
+      )}
+      {favoriteSongs.length > 0 && (
+        <div className='favorite-songs'>
+          <h2>찜한 노래</h2>
+          <SongList data={favoriteSongs} handleFavorite={handleFavorite} />
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -2,20 +2,40 @@ import axios from 'axios';
 import { useRef } from 'react';
 import './index.css';
 
-function SearchBar({ setSongList }) {
+function SearchBar({ setSongList, setShowInitialMessage }) {
   const inputRef = useRef();
 
   const handleSearch = () => {
     axios
       .get(`https://api.manana.kr/karaoke/song/${inputRef.current.value}.json`)
       .then((res) => {
-        setSongList(res.data);
+        if (res.data.length === 0) {
+          setSongList([]);
+        } else {
+          setSongList(res.data);
+          setShowInitialMessage(false); // 검색 결과가 있을 때 메시지를 숨김
+        }
       });
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   return (
     <div className='SearchBar'>
-      <input type='text' className='SearchBar_input' ref={inputRef} />
+      <input
+        type='text'
+        className='SearchBar_input'
+        ref={inputRef}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
+      />
       <button className='SearchBar_button' onClick={handleSearch}>
         검색
       </button>
